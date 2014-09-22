fb922a60cbe35ac26969d7f0b38e4187e7dd5fbd
Ovais Tariq <ovaistariq@gmail.com>
2014-09-22 15:27:35 -0700

    When running the playbook show the list of hosts first that will be affected, and proceed to run the playbook only after confirming with the user

--- deploy_upgraded_mysql_slave/run.sh
+++ deploy_upgraded_mysql_slave/run.sh
@@ -3,4 +3,12 @@
 export PYTHONUNBUFFERED=1
 
 script_root=$(dirname $(readlink -f $0))
-ansible-playbook -i ${script_root}/hosts ${script_root}/main.yml
+ansible-playbook -i ${script_root}/hosts ${script_root}/main.yml --list-hosts
+
+proceed_with_play=
+echo 1>&2 "Would you like to proceed with running the playbook: [yes/no]"
+read proceed_with_play
+if [[ "${proceed_with_play}" == "yes" ]]
+then
+    ansible-playbook -i ${script_root}/hosts ${script_root}/main.yml
+fi
