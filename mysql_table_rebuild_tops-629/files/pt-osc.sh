f064c7cc23593a0f4fd09100b03d07c6aa1f1901
Ovais Tariq <ovaistariq@gmail.com>
2014-10-03 11:57:48 -0700

    Fix parallelization code

--- mysql_table_rebuild_tops-629/files/pt-osc.sh
+++ mysql_table_rebuild_tops-629/files/pt-osc.sh
@@ -16,9 +16,9 @@ fi
 
 osc_config="/root/.pt-online-schema-change.conf"
 
-tmp_dir="/tmp/pt-osc-run-${current_datetime}"
+tmp_dir="/root/tables_rebuild/pt-osc-run-${current_datetime}"
 log_file="${tmp_dir}/pt-osc.log"
-concurrency_control_file="${tmp_dir}/pt-osc.concurrency"
+tbl_failed_list_file="${tmp_dir}/tables_to_rebuild_manually.txt"
 
 mysql_username=$(awk -F= '/user/ {print $2}' ${osc_config})
 mysql_password=$(awk -F= '/password/ {print $2}' ${osc_config})
@@ -40,6 +40,68 @@ sql_to_fetch_tables="select c.table_schema, c.table_name,
                 order by table_size_gb asc"
 
 
+############################################################
+## Parallelization 
+############################################################
+NUM=0
+QUEUE=""
+MAX_NPROC=${concurrency}
+
+function queue() {
+    QUEUE="$QUEUE $1"
+    NUM=$(($NUM+1))
+}
+
+function regeneratequeue() {
+    OLDREQUEUE=$QUEUE
+    QUEUE=""
+    NUM=0
+    for PID in $OLDREQUEUE; do
+        if [ -d /proc/$PID  ] ; then
+            QUEUE="$QUEUE $PID"
+            NUM=$(($NUM+1))
+        fi
+    done
+}
+
+function checkqueue() {
+    OLDCHQUEUE=$QUEUE
+    for PID in $OLDCHQUEUE; do
+        if [ ! -d /proc/$PID ] ; then
+            regeneratequeue # at least one PID has finished
+            break
+        fi
+    done
+}
+
+# Example usage of parallelization functions
+# Main program
+#echo Using $MAX_NPROC parallel threads
+#
+#for ITEM in some_list; do
+#    # DEFINE COMMAND
+#    echo "Running $CMD"
+#
+#    $CMD &
+#    # DEFINE COMMAND END
+#
+#    PID=$!
+#    queue $PID
+#
+#    while (( $NUM >= $MAX_NPROC )); do
+#        checkqueue
+#        sleep 0.4
+#    done
+#done
+
+# wait for all processes to finish before exit
+#    while (( $NUM > 0 )); do
+#        checkqueue
+#        sleep 0.4
+#    done
+## End Parallelization ####################################
+
+
 # Function definitions
 function vlog() {
     datetime=$(date "+%Y-%m-%d %H:%M:%S")
@@ -54,8 +116,7 @@ function test_mysql_access() {
 
     mysqladmin_args="--user=${mysql_username} --password=${mysql_password}"
 
-    if (( $(${mysqladmin_bin} ${mysqladmin_args} ping &> /dev/null; echo $?) != 0 ))
-    then
+    if (( $(${mysqladmin_bin} ${mysqladmin_args} ping &> /dev/null; echo $?) != 0 )); then
         echo "Could not connect to MySQL on $(hostname)"
         exit 2003
     fi
@@ -65,32 +126,11 @@ function test_mysql_access() {
 #    set +x
 }
 
-function increment_alter_threads_count() {
-    # We increment the running counter that shows number of ALTER running
-    local num_running_alters=$(cat ${concurrency_control_file})
-    let num_running_alters=num_running_alters+1
-    echo -n "${num_running_alters}" > ${concurrency_control_file}
-}
-
-function decrement_alter_threads_count() {
-    # We now decrement the counter that shows number of ALTERs running
-    local num_running_alters=$(cat ${concurrency_control_file})
-    let num_running_alters=num_running_alters-1
-    echo -n "${num_running_alters}" > ${concurrency_control_file}
-}
-
-function get_alter_threads_count() {
-    local num_running_alters=$(cat ${concurrency_control_file})
-    echo ${num_running_alters}
-}
-
 function do_online_alter() {
     local db=$1
     local table=$2
     local table_size=$3
     local table_rebuild_log="${tmp_dir}/rebuild-${db}.${table}.log"
-
-    increment_alter_threads_count
 
     vlog "Rebuilding ${db}.${table} - size: ${table_size} - log: ${table_rebuild_log}"
     
@@ -98,24 +138,19 @@ function do_online_alter() {
         --alter "engine=innodb" --alter-foreign-keys-method drop_swap \
         --critical-load Threads_running=5000 --recursion-method none \
         --set-vars "sql_log_bin=0,binlog_format=row" --plugin ${osc_plugin} \
-        --progress percentage,1 --execute &> ${table_rebuild_log}
+        --no-check-plan --progress percentage,1 --dry-run &> ${table_rebuild_log}
 
     ret_code=${PIPESTATUS[0]}
-    if (( ${ret_code} != 0 ))
-    then
+    if (( ${ret_code} != 0 )); then
         vlog "FAILED to rebuild table ${db}.${table}"
     else
         vlog "SUCCESSFULLY rebuilt table ${db}.${table}"
     fi
-
-    decrement_alter_threads_count
 }
 
 # Test that all tools are available
-for tool_bin in ${mysqladmin_bin} ${mysql_bin} ${pt_osc_bin}
-do
-    if (( $(which $tool_bin &> /dev/null; echo $?) != 0 ))
-    then
+for tool_bin in ${mysqladmin_bin} ${mysql_bin} ${pt_osc_bin}; do
+    if (( $(which $tool_bin &> /dev/null; echo $?) != 0 )); then
         echo "Can't find $tool_bin on $(hostname)"
         exit 22 # OS error code  22:  Invalid argument
     fi
@@ -130,15 +165,13 @@ test_mysql_access
 # Setup the directory needed to store temporary files
 mkdir -p ${tmp_dir}
 touch ${log_file}
-echo -n "0" > ${concurrency_control_file}
 
 vlog "Created temporary directory ${tmp_dir} and logging to ${log_file}"
 
 # Now we loop through the list of tables that need to be rebuilt
 mysql_bin="${mysql_bin} --user=${mysql_username} --password=${mysql_password}"
 
-${mysql_bin} -NB information_schema -e "${sql_to_fetch_tables}" | while read line
-do
+${mysql_bin} -NB information_schema -e "${sql_to_fetch_tables}" | while read line; do
     db=$(echo $line | awk '{print $1}')
     table=$(echo $line | awk '{print $2}')
     table_size=$(echo $line | awk '{ 
@@ -156,30 +189,37 @@ do
 
     # We run the alter as a background process to enable parallelization
     do_online_alter ${db} ${table} ${table_size} &
+    
+    pid=$!
+    queue ${pid}
 
-    sleep 1
+    # We sleep a bit for safety purpose to allow the child process running
+    # the alter in bg enough time to fork
+    sleep 0.2
 
     # If we are already running the allowed number of parallel alters
     # then we wait for one of them to finish
-    while (( $(get_alter_threads_count) >= ${concurrency}  ))
-    do
-        sleep 1
+    while (( ${NUM} >= ${MAX_NPROC} )); do
+        checkqueue
+        sleep 0.4
     done
 done
 
-# We wait for all child processes to finish
-while [[ "$(get_alter_threads_count)" != "0" ]]
-do
-    sleep 1
+# We wait for all backgrounded online alters to finish
+while (( ${NUM} > 0 )); do
+    checkqueue
+    sleep 0.4
 done
-
+    
 # Show stats related to table rebuilds failure and success
 vlog "Table rebuild summary:"
 echo "Total tables to rebuild: " $(grep -c Rebuilding ${log_file})
 echo "Tables rebuilt successfully: " $(grep -c SUCCESSFULLY ${log_file})
 echo "Tables rebuilt failed: " $(grep -c FAILED ${log_file})
 echo
-echo "Following tables failed to be rebuilt:"
-echo "--------------------------------------"
-awk '/FAILED/ {print $7}' ${log_file}
+
+# Generate list of tables that need to be rebuilt manually
+grep " was not altered" ${tmp_dir}/rebuild-*.log | cut -d ":" -f 1 | xargs -l -- bash -c 'basename $0 .log' | cut -d '-' -f 2 | sort > ${tbl_failed_list_file}
+
+echo "List of tables that failed to be rebuilt is available at ${tbl_failed_list_file}"
 
