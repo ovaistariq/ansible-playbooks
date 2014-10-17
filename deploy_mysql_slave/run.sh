fc7682f0e74dc8e3b404957619dedfad2233f024
Ovais Tariq <ovaistariq@gmail.com>
2014-10-18 01:04:25 +0500

    Move the variable definition for those needed by the roles to the host inventory file

--- deploy_mysql_slave/run.sh
+++ deploy_mysql_slave/run.sh
@@ -3,12 +3,23 @@
 export PYTHONUNBUFFERED=1
 
 script_root=$(dirname $(readlink -f $0))
-ansible-playbook -i ${script_root}/hosts ${script_root}/main.yml --list-hosts
+
+podname=$1
+if [[ -z ${podname} ]] || [[ ! -e ${script_root}/hosts.${podname} ]]
+then
+    echo "Podname was not provided, could not load the inventory"
+    exit 22
+fi
+
+ansible-playbook -i ${script_root}/hosts.${podname} ${script_root}/main.yml --list-hosts
+
+ret_code=$?
+(( ${ret_code} != 0 )) && exit ${ret_code}
 
 proceed_with_play=
-echo 1>&2 "Would you like to proceed with running the playbook: [yes/no]"
+echo -n 1>&2 "Would you like to proceed with running the playbook [yes/no]: "
 read proceed_with_play
 if [[ "${proceed_with_play}" == "yes" ]]
 then
-    ansible-playbook -i ${script_root}/hosts ${script_root}/main.yml
+    ansible-playbook -i ${script_root}/hosts.${podname} ${script_root}/main.yml
 fi
